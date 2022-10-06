015bac7796e7ef8518093cb936db05198fd5775c
OldBeliver <380506338004@yandex.com>
2022-10-06 10:11:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace NameOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string line = "";
+            string symbolVariants = "@ # $ % ^ & * | /";
+
+            char symbol;
+
+            int lengthLine;
+            int additionalOffset = 4;
+
+
+            Console.WriteLine($"Сегодня на доске почета лучших учеников яЮниор появится Ваше имя.");
+            Console.WriteLine($"Введите свое имя:");
+            name = Console.ReadLine().ToUpper();
+
+            Console.WriteLine($"Введите один символ для рамочки, например: {symbolVariants}");
+            symbol = Convert.ToChar(Console.Read());
+
+            lengthLine = name.Length + additionalOffset;
+
+            for (int i = 0; i < lengthLine; i++)
+            {
+                line += symbol;
+            }
+
+            Console.WriteLine(line);
+            Console.WriteLine($"{symbol} {name} {symbol}");
+            Console.WriteLine(line);
+        }
+    }
+}
